19c258936aaa42e4ebed603e42f8747626185b17
Ade Ismail Siregar <adeismailbox@gmail.com>
2026-08-06 07:45:00 +0700

    docs(header): update template attribution to SLiMS Bulian 9.8 Default Template by Waris Agung Widodo

--- parts/header.php
+++ parts/header.php
@@ -3,7 +3,7 @@
 # Rasamala SLiMS Template
 # ======================================================================
 # @Author: Ade Ismail Siregar <adeismailbox@gmail.com>
-# @Based on: Classic SLiMS Template by Waris Agung Widodo <ido.alit@gmail.com>
+# @Based on: SLiMS Bulian 9.8 Default Template by Waris Agung Widodo <ido.alit@gmail.com>
 # @Date: 2026-08-06T07:43:00+07:00
 # ======================================================================
 -->
